6ebdb0b3346c36d9a95a8bd75caf842513dd2bff
Mike Mariano <mike@uiuxartist.com>
2017-01-27 00:29:05 -0800

    Hook up input fields and checkboxes

---
+++ MyLastRun/PhotoPickerViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PhotoPickerViewController.swift
+//  My Last Run
+//
+//  Created by Mike Mariano on 1/27/17.
+//  Copyright © 2017 Mike Mariano. All rights reserved.
+//
+
+import UIKit
+
+class PhotoPickerViewController: UIViewController {
+
+    @IBOutlet weak var PhotoTitle: UITextField!
+    
+    var statsViewController: ViewController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        PhotoTitle.text = statsViewController?.TitleInput.text
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MyLastRun/ViewController.swift
+++ MyLastRun/ViewController.swift
@@ -8,18 +8,251 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // photo overlay fields
+    
+    
+    // title variables
+    @IBOutlet weak var BtnTitleCheckbox: UIButton!
+    @IBOutlet weak var TitleInput: UITextField!
+    
+    // distance variables
+    @IBOutlet weak var BtnDistanceCheckbox: UIButton!
+    @IBOutlet weak var DistanceInput: UITextField!
+    
+    // time variables
+    @IBOutlet weak var BtnTimeCheckbox: UIButton!
+    @IBOutlet weak var TimeInput: UITextField!
+    
+    // location variables
+    @IBOutlet weak var BtnLocationCheckbox: UIButton!
+    @IBOutlet weak var LocationInput: UITextField!
+    
+    // date variables
+    @IBOutlet weak var DateInput: UITextField!
+    @IBOutlet weak var BtnDateCheckbox: UIButton!
+    
+    // checked variables
+    var titleIsChecked: Bool!
+    var distanceIsChecked: Bool!
+    var timeIsChecked: Bool!
+    var locationIsChecked: Bool!
+    var dateIsChecked: Bool!
+    
+    // checkbox variables
+    var checkbox = UIImage(named: "Checked")
+    var unCheckbox = UIImage(named: "Unchecked")
+    
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Initialize variables
+        titleIsChecked = true
+        distanceIsChecked = true
+        timeIsChecked = true
+        locationIsChecked = true
+        dateIsChecked = true
+        
+        // Handle the text field's user input through delegate callbacks.
+        TitleInput.delegate = self;
+        DistanceInput.delegate = self;
+        TimeInput.delegate = self;
+        LocationInput.delegate = self;
+        DateInput.delegate = self;
+        
+        // Date picker
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: self.view.frame.size.height/6, width: self.view.frame.size.width, height: 40.0))
+        
+        toolBar.layer.position = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height-20.0)
+        
+        toolBar.barStyle = UIBarStyle.blackTranslucent
+        
+        toolBar.tintColor = UIColor.white
+        
+        toolBar.backgroundColor = UIColor.black
+        
+        
+        let todayBtn = UIBarButtonItem(title: "Today", style: UIBarButtonItemStyle.plain, target: self, action: #selector(ViewController.tappedToolBarBtn))
+        
+        let okBarBtn = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(ViewController.donePressed))
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: self, action: nil)
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width / 3, height: self.view.frame.size.height))
+        
+        label.font = UIFont(name: "Helvetica", size: 12)
+        
+        label.backgroundColor = UIColor.clear
+        
+        label.textColor = UIColor.white
+        
+        label.text = "Select the Date"
+        
+        label.textAlignment = NSTextAlignment.center
+        
+        let textBtn = UIBarButtonItem(customView: label)
+        
+        toolBar.setItems([todayBtn,flexSpace,textBtn,flexSpace,okBarBtn], animated: true)
+        
+        DateInput.inputAccessoryView = toolBar
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func donePressed(_ sender: UIBarButtonItem) {
+        
+        DateInput.resignFirstResponder()
+        
+    }
+    
+    func tappedToolBarBtn(_ sender: UIBarButtonItem) {
+        
+        let dateformatter = DateFormatter()
+        
+        dateformatter.dateStyle = DateFormatter.Style.medium
+        
+        dateformatter.timeStyle = DateFormatter.Style.none
+        
+        DateInput.text = dateformatter.string(from: Date())
+        
+        DateInput.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func textFieldEditing(_ sender: UITextField) {
+        
+        let datePickerView: UIDatePicker = UIDatePicker()
+        
+        datePickerView.datePickerMode = UIDatePickerMode.date
+        
+        sender.inputView = datePickerView
+        
+        DateInput.addTarget(self, action: #selector(ViewController.datePickerValueChanged(_:)), for: UIControlEvents.valueChanged)
+        
+    }
+    
+    func datePickerValueChanged(_ sender: UIDatePicker) {
+        
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = DateFormatter.Style.full
+        
+        dateFormatter.timeStyle = DateFormatter.Style.none
+        
+        DateInput.text = dateFormatter.string(from: sender.date)
+        
+    }
 
+    
+    @IBAction func TapTitleCheckbox(_ sender: UIButton) {
+        
+        if titleIsChecked == true {
+            titleIsChecked = false
+        }else{
+            titleIsChecked = true
+        }
+        
+        if titleIsChecked == true {
+            BtnTitleCheckbox.setImage(checkbox, for: UIControlState.normal)
+        } else {
+            BtnTitleCheckbox.setImage(unCheckbox, for: UIControlState.normal)
+        }
+
+    }
+    
+    @IBAction func TapDistanceCheckbox(_ sender: UIButton) {
+        
+        if timeIsChecked == true {
+            timeIsChecked = false
+        }else{
+            timeIsChecked = true
+        }
+        
+        if timeIsChecked == true {
+            BtnDistanceCheckbox.setImage(checkbox, for: UIControlState.normal)
+        } else {
+            BtnDistanceCheckbox.setImage(unCheckbox, for: UIControlState.normal)
+        }
+        
+    }
+    
+    @IBAction func TapTimeCheckbox(_ sender: UIButton) {
+        
+        if distanceIsChecked == true {
+            distanceIsChecked = false
+        }else{
+            distanceIsChecked = true
+        }
+        
+        if distanceIsChecked == true {
+            BtnTimeCheckbox.setImage(checkbox, for: UIControlState.normal)
+        } else {
+            BtnTimeCheckbox.setImage(unCheckbox, for: UIControlState.normal)
+        }
+        
+    }
+    
+    @IBAction func TapLocationCheckbox(_ sender: UIButton) {
+        
+        if locationIsChecked == true {
+            locationIsChecked = false
+        }else{
+            locationIsChecked = true
+        }
+        
+        if locationIsChecked == true {
+            BtnLocationCheckbox.setImage(checkbox, for: UIControlState.normal)
+        } else {
+            BtnLocationCheckbox.setImage(unCheckbox, for: UIControlState.normal)
+        }
+        
+    }
+    
+    @IBAction func DateInputEdit(_ sender: UITextField) {
+        let datePickerView:UIDatePicker = UIDatePicker()
+        
+        datePickerView.datePickerMode = UIDatePickerMode.dateAndTime
+        
+        sender.inputView = datePickerView
+        
+        datePickerView.addTarget(self, action: #selector(ViewController.datePickerValueChanged(_:)), for: UIControlEvents.valueChanged)
+    }
+    
+    @IBAction func DateLocationCheckbox(_ sender: UIButton) {
+        
+        if dateIsChecked == true {
+            dateIsChecked = false
+        }else{
+            dateIsChecked = true
+        }
+        
+        if dateIsChecked == true {
+            BtnDateCheckbox.setImage(checkbox, for: UIControlState.normal)
+        } else {
+            BtnDateCheckbox.setImage(unCheckbox, for: UIControlState.normal)
+        }
+        
+    }
+    
+    
 
 }
 
