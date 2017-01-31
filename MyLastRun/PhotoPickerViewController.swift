8889dc3aa7d62746b6858ae7021a4ad9516d402c
Mike Mariano <mike@uiuxartist.com>
2017-01-30 20:51:05 -0800

    Add photo picker

--- MyLastRun/PhotoPickerViewController.swift
+++ MyLastRun/PhotoPickerViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class PhotoPickerViewController: UIViewController {
+class PhotoPickerViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var photoImageView: UIImageView!
     
     // reference the new text field
     @IBOutlet weak var PhotoTitle: UITextField!
@@ -26,15 +29,47 @@ class PhotoPickerViewController: UIViewController {
         
         PhotoTitle.text = userTitleText
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func seletImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+
     
     @IBAction func backToStats(_ sender: UIBarButtonItem) {
         self.performSegue(withIdentifier: "unwindToStats", sender: self)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
 
